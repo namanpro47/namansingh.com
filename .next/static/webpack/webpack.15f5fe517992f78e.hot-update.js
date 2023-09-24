67d60d054075fd20087ca5f886e767cae9cc95fd
Naman Singh <naman.pro47@gmail.com>
2023-09-23 21:06:05 -0700

    minor fixes

--- .next/static/webpack/webpack.15f5fe517992f78e.hot-update.js
+++ .next/static/webpack/webpack.15f5fe517992f78e.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "f98665e9a29afabc"; }
+/******/ 	__webpack_require__.h = function() { return "be19a45514964ae8"; }
 /******/ }();
 /******/ 
 /******/ }
